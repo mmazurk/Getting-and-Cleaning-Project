0a396734a3c02bf1112d1feaecca84113a0ff396
Mark <markmazurk@gmail.com>
2015-06-10 16:56:40 -0700

    First commit.

---
+++ run_analysis.R
@@ -0,0 +1,26 @@
+
+
+## You should create one R script called run_analysis.R that does the following. 
+
+# Merges the training and the test sets to create one data set.
+
+subject_train shows the participant number
+X_train is the data of which elements 1 - 6 of 561 are the mean and std
+
+1 tBodyAcc-mean()-X
+2 tBodyAcc-mean()-Y
+3 tBodyAcc-mean()-Z
+4 tBodyAcc-std()-X
+5 tBodyAcc-std()-Y
+6 tBodyAcc-std()-Z
+
+y_train is the activity label (which can be found in a separate file called activity_labels)
+
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+# Uses descriptive activity names to name the activities in the data set
+
+# Appropriately labels the data set with descriptive variable names. 
+
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
