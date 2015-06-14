78c29a8299882756d27523d4982faf020b6091a9
Mark <markmazurk@gmail.com>
2015-06-14 14:30:32 -0700

    Got the initial code working so it extracts the right data, creates two clean datasets. Will add more later.

---
+++ README.md
@@ -0,0 +1,31 @@
+---
+title: "ReadMe"
+author: "Mark Mazurkiewicz"
+date: "June 10, 2015"
+output: html_document
+---
+
+# Description of the Dataset 
+
+The most difficult part of this dataset is understanding the stucture and content of the data. For the purposes of the assignment we need the following variables, which are split into both the test and train folders.
+
+*subject_train* shows the participant number
+
+*y_train* is the activity label (which can be found in a separate file called activity_labels)
+
+*X_train* is the vector data of which elements 1 - 6 (out of of 561) are the mean and standard deviation as follows:
+
+```
+1 tBodyAcc-mean()-X
+2 tBodyAcc-mean()-Y
+3 tBodyAcc-mean()-Z
+4 tBodyAcc-std()-X
+5 tBodyAcc-std()-Y
+6 tBodyAcc-std()-Z
+```
+
+# Extracted Dataset
+
+
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,73 @@
 
 
-## You should create one R script called run_analysis.R that does the following. 
+## Getting and Cleaning Data
+## Course Project
+## by Mark Mazurkiewicz
 
-# Merges the training and the test sets to create one data set.
 
-subject_train shows the participant number
-X_train is the data of which elements 1 - 6 of 561 are the mean and std
+# First let's list the files and store the names
+(fileList <- list.files("../UCI HAR Dataset/test", full.names = TRUE))
 
-1 tBodyAcc-mean()-X
-2 tBodyAcc-mean()-Y
-3 tBodyAcc-mean()-Z
-4 tBodyAcc-std()-X
-5 tBodyAcc-std()-Y
-6 tBodyAcc-std()-Z
+# Now let's get the subject ID number
+subjectTest <- read.table(fileList[2], header = FALSE, col.names = c("Subject"), stringsAsFactors = FALSE)
 
-y_train is the activity label (which can be found in a separate file called activity_labels)
+# And let's get activity ID number
+activityTest <- read.table(fileList[4], header = FALSE, col.names = c("Activity"), stringsAsFactors = FALSE)
 
-# Extracts only the measurements on the mean and standard deviation for each measurement. 
+# We only want to extract the mean and standard deviation and these are located in the first six columns of the data. We will use colClasses to do this:
+dataTest <- read.table(fileList[3], header = FALSE, colClasses = c(rep("numeric", 6), rep("NULL", 555)))
 
-# Uses descriptive activity names to name the activities in the data set
 
-# Appropriately labels the data set with descriptive variable names. 
+#############################################
 
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+### Mark Note: Is there a way to figure out how many columns therer would be so I didn't have to hardcode the "555" number above? ###
+
+#############################################
+
+# Okay, so we have all the data we need, now let's rename the variables
+names(subjectTest)[1] <- "ParticipantID"
+names(activityTest)[1] <- "ActivityID"
+names(dataTest)[1:6] <- c("MeanXAccel", "MeanYAccel", "MeanZAccel", "SdXAccel", "SdYAccel", "SdZAccel")
+
+# Et voila -- let's combine data frames them to make a nice dataset
+finalData <- cbind(subjectTest, activityTest, dataTest)
+
+# But we have to label this as test data and not train data
+finalDataTest <- data.frame(finalData, DataType = c("test"))
+
+
+
+# Okay, let's do it all again for the train data
+
+# First let's list the files and store the names
+(fileList <- list.files("../UCI HAR Dataset/train", full.names = TRUE))
+
+# Now let's get the subject ID number
+subjectTrain <- read.table(fileList[2], header = FALSE, col.names = c("Subject"), stringsAsFactors = FALSE)
+
+# And let's get activity ID number
+activityTrain <- read.table(fileList[4], header = FALSE, col.names = c("Activity"), stringsAsFactors = FALSE)
+
+# We only want to extract the mean and standard deviation and these are located in the first six columns of the data. We will use colClasses to do this:
+dataTrain <- read.table(fileList[3], header = FALSE, colClasses = c(rep("numeric", 6), rep("NULL", 555)))
+
+# Okay, so we have all the data we need, now let's rename the variables
+names(subjectTrain)[1] <- "ParticipantID"
+names(activityTrain)[1] <- "ActivityID"
+names(dataTrain)[1:6] <- c("MeanXAccel", "MeanYAccel", "MeanZAccel", "SdXAccel", "SdYAccel", "SdZAccel")
+
+# Et voila -- let's combine data frames them to make a nice dataset
+finalData <- cbind(subjectTrain, activityTrain, dataTrain)
+
+# But we have to label this as train data and not test data
+finalDataTrain <- data.frame(finalData, DataType = c("train"))
+
+# Okay, so we now have two clean datasets that we need to merge.
+# These are as follows: finalDataTest and finalDataTrain
+
+# Let's merge them and then rename the ActivityID numbers to something more interesting.
+
+# And then we are done.
+
+
 
